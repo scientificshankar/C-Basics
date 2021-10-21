89bf8736072b771f99cd65f922155193690a9c77
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-10-21 10:21:08 +0530

    Add files via upload

---
+++ enter program.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+ 	printf("roll.no:20mh5a0333\n");
+	 printf("name:ravi shankar\n");
+	printf("branch:mechanical\n");
+	printf("college:aditya college of engineering");
+}
