872e2fde98174fd4aa385ef76b740075ea7d328c
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-10-23 10:57:09 +0530

    Add files via upload

---
+++ 9. decimal addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{ 
+// variable declaration
+float number1, number2, sum;
+
+// initialization
+number1 = 2.5;
+number2 = 3.0;
+// calculation 
+sum = number1+number2;
+
+//output
+printf("sum of %f and %f is %f",number1,number2,sum);
+
+
+}
