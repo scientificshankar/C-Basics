c8ef1c8ebeb8d75d04578bd2ad2a90355d7e418b
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-10-23 10:41:15 +0530

    Add files via upload

---
+++ 8. addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{ 
+// variable declaration
+int number1, number2, sum;
+
+// initialization
+number1 = 25;
+number2 = 30;
+// calculation 
+sum = number1+number2;
+
+//output
+printf("sum of %d and %d is %d",number1,number2,sum);
+
+
+}
