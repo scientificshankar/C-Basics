12d2c8037c55e21dcda175fab699dbff28b90689
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-11-03 21:05:38 -0700

    Add files via upload

---
+++ 10 addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{ 
+// variable declaration
+int physics, chemistry,maths,english,sum;
+
+// initialization
+physics = 40;
+chemistry = 60;
+maths =70;
+english = 50;
+// calculation 
+sum = physics=chemistry=maths=english;
+
+//output
+printf(" marks in physics\t: %d\n marks in chemistry\t: %d\n marks in maths \t: %d\n marks in english\t: %d\n total marks \t \t: %d",physics,chemistry,maths,english,sum);
+}
