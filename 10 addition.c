cd87b51b1c08f676e8f17c4ba1c7faab393817c7
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-11-05 09:39:59 +0530

    Update 10 addition.c

--- 10 addition.c
+++ 10 addition.c
@@ -10,7 +10,7 @@ chemistry = 60;
 maths =70;
 english = 50;
 // calculation 
-sum = physics=chemistry=maths=english;
+sum = physics+chemistry+maths+english;
 
 //output
 printf(" marks in physics\t: %d\n marks in chemistry\t: %d\n marks in maths \t: %d\n marks in english\t: %d\n total marks \t \t: %d",physics,chemistry,maths,english,sum);
