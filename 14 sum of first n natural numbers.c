b88cf57f79dcb1d4edc89450573ffe449582e638
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-11-11 10:30:13 +0530

    Add files via upload

---
+++ 14 sum of first n natural numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main ()
+ {
+ 	int n,k,l,j;
+ 	n=100;
+ 	k= n*(n+1)/2;
+ 	l=n*(n+1);
+ 	j=n*n;
+ 
+ 	
+ 	printf("sum of first %d natural numbers is %d\n" ,n,k);
+ 	
+ 	printf("sum of first %d even natural numbers is %d\n", n, l);
+ 	
+ 	printf("sum of first %d odd numbers is %d", n, j);
+ 	
+ 	
+ }
+ 
+  
+ 		
+ 	 
+ 	 
