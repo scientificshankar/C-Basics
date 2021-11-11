ceb1ba4a32c273d3e42c5dd0d9c92b8a9bc9a15d
scientificshankar <92074758+scientificshankar@users.noreply.github.com>
2021-11-11 10:29:01 +0530

    Add files via upload

---
+++ 13 scanf perimeter of square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter a side value");
+	scanf("%d",&a);
+	printf("perimeter of the square = %d\n", 4*a);
+	
+	printf("area of the square = %d", a*a);
+	
+	
+
+}
